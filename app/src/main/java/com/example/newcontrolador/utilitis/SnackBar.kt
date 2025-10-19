d53aab4d00de7abf4336d0239d00f0f94cb2e3d4
lautaro <tomas.lautaro.ardiles@gmail.com>
2025-10-18 21:12:40 -0300

    cambiois en la mueestra de mensajes

--- app/src/main/java/com/example/newcontrolador/utilitis/SnackBar.kt
+++ app/src/main/java/com/example/newcontrolador/utilitis/SnackBar.kt
@@ -3,16 +3,21 @@ package com.example.newcontrolador.utilitis
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CustomSnackbar(snackbarData: SnackbarData) {
-	Snackbar(
-		snackbarData = snackbarData,
-		contentColor = MaterialTheme.colorScheme.background,
-		shape = RoundedCornerShape(10.dp),
-		containerColor = MaterialTheme.colorScheme.secondary
-	)
+fun CustomSnackbar(message: String) {
+    Snackbar(
+        containerColor = MaterialTheme.colorScheme.secondary,
+        contentColor = MaterialTheme.colorScheme.background,
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.background
+        )
+    }
 }
