93949ddd3272c5805699ddfab7295b986977b131
lardiles <tomas.lautaro.ardiles@gmail.com>
2026-06-08 20:19:47 -0300

    Me olvide de cambiar ButtonConfig.kt (los valores por defecto bueno)

--- app/src/main/java/com/example/newcontrolador/connection/data/ButtonConfig.kt
+++ app/src/main/java/com/example/newcontrolador/connection/data/ButtonConfig.kt
@@ -1,7 +1,7 @@
 package com.example.newcontrolador.connection.data
 
 data class ButtonConfig(
-	val width: Float = 165f,
-	val height: Float = 150f,
+	val width: Float = 1f/4f,
+	val height: Float = 1f/2f,
 	val padding: Float = 0f
 )
